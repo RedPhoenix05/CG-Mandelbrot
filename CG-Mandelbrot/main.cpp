7a8f4a7aa13eb216dd95f245fabd0da7762a85e0
RedPhoenix05 <anson.fry@gmail.com>
2026-04-23 00:13:20 -0500

    Update main.cpp

--- CG-Mandelbrot/main.cpp
+++ CG-Mandelbrot/main.cpp
@@ -11,6 +11,11 @@
 
 using Microsoft::WRL::ComPtr;
 
+// global vars
+ComPtr<ID3D12Device> device;
+ComPtr<ID3D12CommandQueue> commandQueue;
+
+// enable debug layer
 void EnableDebugLayer() {
 #if defined(_DEBUG)
     ComPtr<ID3D12Debug> debug;
@@ -20,29 +25,24 @@ void EnableDebugLayer() {
 #endif
 }
 
-int main()
+// create device for GPU connection
+void CreateDevice()
 {
-	// create window
-    glfwInit();
-    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-    GLFWwindow* window = glfwCreateWindow(1280, 720, "DX12", nullptr, nullptr);
-
-    // enable debug layer
-    EnableDebugLayer();
-
-    // create device for GPU connection
-    ComPtr<ID3D12Device> device;
     D3D12CreateDevice(nullptr, D3D_FEATURE_LEVEL_11_0, IID_PPV_ARGS(&device));
+}
 
-    // setup command queue
-    ComPtr<ID3D12CommandQueue> queue;
-
+// setup command queue
+void SetupCommandQueue()
+{
     D3D12_COMMAND_QUEUE_DESC desc = {};
     desc.Type = D3D12_COMMAND_LIST_TYPE_DIRECT;
 
-    device->CreateCommandQueue(&desc, IID_PPV_ARGS(&queue));
+    device->CreateCommandQueue(&desc, IID_PPV_ARGS(&commandQueue));
+}
 
-    // create swap chain
+// create swap chain
+void CreateSwapChain(GLFWwindow* window)
+{
     ComPtr<IDXGISwapChain1> swapchain1;
     ComPtr<IDXGISwapChain3> swapchain;
 
@@ -57,7 +57,7 @@ int main()
     CreateDXGIFactory1(IID_PPV_ARGS(&factory));
 
     factory->CreateSwapChainForHwnd(
-        queue.Get(),
+        commandQueue.Get(),
         glfwGetWin32Window(window), // requires GLFW native access
         &scDesc,
         nullptr,
@@ -66,8 +66,11 @@ int main()
     );
 
     swapchain1.As(&swapchain);
+}
 
-    // render target view
+// render target view
+void RenderTargetView()
+{
     ComPtr<ID3D12DescriptorHeap> rtvHeap;
 
     D3D12_DESCRIPTOR_HEAP_DESC heapDesc = {};
@@ -75,14 +78,33 @@ int main()
     heapDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_RTV;
 
     device->CreateDescriptorHeap(&heapDesc, IID_PPV_ARGS(&rtvHeap));
+}
 
-    // command allocator
+// command allocator
+void CreateCommandAllocator()
+{
     ComPtr<ID3D12CommandAllocator> allocator;
     ComPtr<ID3D12GraphicsCommandList> cmdList;
 
     device->CreateCommandAllocator(D3D12_COMMAND_LIST_TYPE_DIRECT, IID_PPV_ARGS(&allocator));
 
     device->CreateCommandList(0, D3D12_COMMAND_LIST_TYPE_DIRECT, allocator.Get(), nullptr, IID_PPV_ARGS(&cmdList));
+}
+
+int main()
+{
+	// create window
+    glfwInit();
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    GLFWwindow* window = glfwCreateWindow(1280, 720, "DX12", nullptr, nullptr);
+
+    
+    EnableDebugLayer();
+    CreateDevice();
+    SetupCommandQueue();
+    CreateSwapChain(window);
+	RenderTargetView();
+    CreateCommandAllocator();
 
     // main loop
     while (!glfwWindowShouldClose(window))
