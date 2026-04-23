42b77fb363c82a80091eb1625d8153d629e64e90
RedPhoenix05 <anson.fry@gmail.com>
2026-04-22 23:41:55 -0500

    Enable DX12 debug layer

--- CG-Mandelbrot/main.cpp
+++ CG-Mandelbrot/main.cpp
@@ -3,8 +3,29 @@
 // when creating window
 //glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
 
+
+
+// DX12 debug layer
+#include <d3d12.h>
+#include <dxgi1_6.h>
+#include <wrl.h>
+
+using Microsoft::WRL::ComPtr;
+
+void EnableDebugLayer() {
+#if defined(_DEBUG)
+    ComPtr<ID3D12Debug> debug;
+    if (SUCCEEDED(D3D12GetDebugInterface(IID_PPV_ARGS(&debug)))) {
+        debug->EnableDebugLayer();
+    }
+#endif
+}
+
 int main()
 {
+	// enable debug layer
+	EnableDebugLayer();
+
     glfwInit();
     glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
 
