d621d8295f4c54d141100fb0a897e8bbd4769d4e
RedPhoenix05 <anson.fry@gmail.com>
2026-04-24 19:10:51 -0500

    Fullscreen pass
    full-screen shaded single triangle. twice as efficient as a quad for the screen size.

--- CG-Mandelbrot/main.cpp
+++ CG-Mandelbrot/main.cpp
@@ -6,10 +6,12 @@
 #include <GLFW/glfw3native.h>
 
 #include <d3d12.h>
+#include <d3dcompiler.h>
 #include <dxgi1_6.h>
 #include <wrl.h>
 #include <Windows.h>
 
+#include <string>
 #include <utility>
 
 using Microsoft::WRL::ComPtr;
@@ -24,8 +26,36 @@ UINT rtvDescriptorSize;
 ComPtr<ID3D12CommandAllocator> commandAllocator;
 ComPtr<ID3D12GraphicsCommandList> commandList;
 ComPtr<ID3D12Fence> fence;
+ComPtr<ID3D12RootSignature> rootSignature;
+ComPtr<ID3D12PipelineState> pipelineState;
 UINT64 fenceValue = 0;
 HANDLE fenceEvent = nullptr;
+
+const char* kFullscreenShaderSource = R"(
+struct VSOutput
+{
+    float4 position : SV_Position;
+    float2 uv : TEXCOORD0;
+};
+
+VSOutput VSMain(uint vertexId : SV_VertexID)
+{
+    float2 pos;
+    if (vertexId == 0) pos = float2(-1.0, -1.0);
+    else if (vertexId == 1) pos = float2(-1.0, 3.0);
+    else pos = float2(3.0, -1.0);
+
+    VSOutput output;
+    output.position = float4(pos, 0.0, 1.0);
+    output.uv = pos * 0.5 + 0.5;
+    return output;
+}
+
+float4 PSMain(VSOutput input) : SV_Target
+{
+    return float4(input.uv, 0.15, 1.0);
+}
+)";
 
 // enable debug layer
 void EnableDebugLayer()
@@ -120,6 +150,68 @@ void CreateFenceObjects()
     fenceEvent = CreateEvent(nullptr, FALSE, FALSE, nullptr);
 }
 
+ComPtr<ID3DBlob> CompileShader(const char* entryPoint, const char* target)
+{
+    UINT compileFlags = 0;
+#if defined(_DEBUG)
+    compileFlags = D3DCOMPILE_DEBUG | D3DCOMPILE_SKIP_OPTIMIZATION;
+#endif
+
+    ComPtr<ID3DBlob> bytecode;
+    ComPtr<ID3DBlob> errors;
+    D3DCompile(
+        kFullscreenShaderSource,
+        strlen(kFullscreenShaderSource),
+        nullptr,
+        nullptr,
+        nullptr,
+        entryPoint,
+        target,
+        compileFlags,
+        0,
+        &bytecode,
+        &errors);
+
+    return bytecode;
+}
+
+void CreateFullscreenPipeline()
+{
+    D3D12_ROOT_SIGNATURE_DESC rootSigDesc = {};
+    rootSigDesc.Flags = D3D12_ROOT_SIGNATURE_FLAG_ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT;
+
+    ComPtr<ID3DBlob> serializedRootSig;
+    ComPtr<ID3DBlob> rootSigErrors;
+    D3D12SerializeRootSignature(&rootSigDesc, D3D_ROOT_SIGNATURE_VERSION_1, &serializedRootSig, &rootSigErrors);
+    device->CreateRootSignature(0, serializedRootSig->GetBufferPointer(), serializedRootSig->GetBufferSize(), IID_PPV_ARGS(&rootSignature));
+
+    ComPtr<ID3DBlob> vsBytecode = CompileShader("VSMain", "vs_5_0");
+    ComPtr<ID3DBlob> psBytecode = CompileShader("PSMain", "ps_5_0");
+
+    D3D12_GRAPHICS_PIPELINE_STATE_DESC psoDesc = {};
+    psoDesc.pRootSignature = rootSignature.Get();
+    psoDesc.VS = { vsBytecode->GetBufferPointer(), vsBytecode->GetBufferSize() };
+    psoDesc.PS = { psBytecode->GetBufferPointer(), psBytecode->GetBufferSize() };
+    psoDesc.BlendState.AlphaToCoverageEnable = FALSE;
+    psoDesc.BlendState.IndependentBlendEnable = FALSE;
+    psoDesc.BlendState.RenderTarget[0].BlendEnable = FALSE;
+    psoDesc.BlendState.RenderTarget[0].LogicOpEnable = FALSE;
+    psoDesc.BlendState.RenderTarget[0].RenderTargetWriteMask = D3D12_COLOR_WRITE_ENABLE_ALL;
+    psoDesc.RasterizerState.FillMode = D3D12_FILL_MODE_SOLID;
+    psoDesc.RasterizerState.CullMode = D3D12_CULL_MODE_NONE;
+    psoDesc.RasterizerState.FrontCounterClockwise = FALSE;
+    psoDesc.RasterizerState.DepthClipEnable = TRUE;
+    psoDesc.DepthStencilState.DepthEnable = FALSE;
+    psoDesc.DepthStencilState.StencilEnable = FALSE;
+    psoDesc.SampleMask = UINT_MAX;
+    psoDesc.PrimitiveTopologyType = D3D12_PRIMITIVE_TOPOLOGY_TYPE_TRIANGLE;
+    psoDesc.NumRenderTargets = 1;
+    psoDesc.RTVFormats[0] = DXGI_FORMAT_R8G8B8A8_UNORM;
+    psoDesc.SampleDesc.Count = 1;
+
+    device->CreateGraphicsPipelineState(&psoDesc, IID_PPV_ARGS(&pipelineState));
+}
+
 void WaitForGpu()
 {
     const UINT64 signalValue = fenceValue;
@@ -141,7 +233,7 @@ void Render()
 
     // reset command system
     commandAllocator->Reset();
-    commandList->Reset(commandAllocator.Get(), nullptr);
+    commandList->Reset(commandAllocator.Get(), pipelineState.Get());
 
     // get current RTV handle
     D3D12_CPU_DESCRIPTOR_HANDLE rtvHandle =
@@ -176,6 +268,10 @@ void Render()
     const float color[4] = { 0.1f, 0.2f, 0.4f, 1.0f };
     commandList->ClearRenderTargetView(rtvHandle, color, 0, nullptr);
 
+    commandList->SetGraphicsRootSignature(rootSignature.Get());
+    commandList->IASetPrimitiveTopology(D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+    commandList->DrawInstanced(3, 1, 0, 0);
+
     // transition back RENDER_TARGET -> PRESENT
     std::swap(barrier.Transition.StateBefore, barrier.Transition.StateAfter);
     commandList->ResourceBarrier(1, &barrier);
@@ -204,6 +300,7 @@ int main()
     RenderTargetView();
     CreateCommandAllocator();
     CreateFenceObjects();
+    CreateFullscreenPipeline();
 
     // main loop
     while (!glfwWindowShouldClose(window))
